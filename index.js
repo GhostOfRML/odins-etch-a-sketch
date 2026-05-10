41a96fd5be7e39e123166bc44f670c31de4021f1
GhosOfRML <ghostofrml9848@gmail.com>
2026-05-10 13:16:07 +0300

    Added a button and its js code Added a random rgb generator

--- index.js
+++ index.js
@@ -1,12 +1,29 @@
+const button = document.querySelector(".prompt")
+const screen = document.querySelector(".sketch-screen");
+
+
+button.addEventListener("click" , () => {
+    screen.innerHTML= "" ;
+    createGrid(prompt("Tell me a number up to 100"))
+});
+function colorGen() {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+   let rgb = (" + r + "," + g + "," + b + ");
+};
+
 function createGrid(size) {
-    let screen = document.querySelector(".sketch-screen")
     
     for (let i = 0 ;i <size ;i++ ){
-        if (size > 101){
-        alert ("The maximum size is 100!")
+        if (size > 100){
+        alert ("The maximum size is 100!");
+        return;
         }
         else {
         let column =document.createElement("div");
+        
+        )
         column.classList.add("column")
         for (let j = 1;j <= size; j++ ){
             let row = document.createElement("div");
@@ -18,4 +35,9 @@ function createGrid(size) {
     
     }
 };
-createGrid(prompt("Tell me a number up to 100"))
+
+
+screen.addEventListener("mouseover",() => {
+    changeBackgroundColor();
+} )
+
